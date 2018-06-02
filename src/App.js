2b2ed88a0a456356880087b5a0cba5b6143e45ee
richard.ng <richard.chihong.ng@gmail.com>
2018-06-02 17:40:58 +0800

    image classification

--- src/App.js
+++ src/App.js
@@ -1,185 +1,175 @@
 import React, { Component } from 'react';
-import './App.css';
 import * as tf from '@tensorflow/tfjs';
 import * as _ from 'lodash'
-import { getColorMatrixTextureShapeWidthHeight } from '@tensorflow/tfjs-core/dist/kernels/webgl/tex_util';
+import { div } from '@tensorflow/tfjs';
+import { IMAGENET_CLASSES } from './IMAGENET_classes';
 
-const twitterBackgroundColor = '#29A0EC'
+const XCEPTION_MODEL_PATH = 'xeception/model.json'
+// const XCEPTION_MODEL_PATH = 'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
+
+const IMAGE_SIZE = 299
+// const IMAGE_SIZE = 224
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.BODY_LENGTH = 30
-    this.DIM = 10000
     this.state = {
-      body_text: '',
-      ready: false,
-      raw_tokenized: null,
-      output_tensor: null,
-      score: null
+      image_src: null,
+      status_text: '',
+      predictions: []
     }
 
-    this.handleBodyText = this.handleBodyText.bind(this)
+    this.handleFileInput = this.handleFileInput.bind(this);
+    this.handleLoadModelClick = this.handleLoadModelClick.bind(this);
   }
 
-  one_hot_encode_sequences(sequences, dimension=this.DIM) {
-    let buffer = tf.buffer([sequences.length, dimension])
-    sequences.map((seq, row) => {
-      seq.map((idx, i) => {
-        buffer.set(1, row, idx)
-      })
-    })
+  handleLoadModelClick(e) {
+    console.log('loading model...')
+    this.setState({ status_text: 'loading model...' })
 
-    buffer.toTensor().print()
-
-    return buffer.toTensor()
-  }
-
-  handleBodyText(e) {    
-    const body_text = e.target.value
-    this.setState({ body_text })
-
-    if (!this.state.ready) {
-      console.log('not ready')
-      return;
-    }
-
-    // Generate raw_tokenized
-    let split = [];
-    let userText_tokenzied = [];
-    let lines = body_text.split('\n')
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      const words = line.split(' ')
-      for (let j = 0; j < words.length; j++) {
-        const word = words[j];
-        split.push(word)
-      }
-    }
-    for (let i = 0; i < split.length; i++) {
-      const word = split[i];
-      if (this.word2idx.hasOwnProperty(word)) {
-        const idx = this.word2idx[word]
-        userText_tokenzied.push(idx)
-      }
-    }
-    
-    // Pad it by BODY_LENGTH
-    let raw_tokenized = Array(this.BODY_LENGTH).fill(0);
-    if (userText_tokenzied.length > this.BODY_LENGTH) {
-      raw_tokenized = userText_tokenzied.slice(0, this.BODY_LENGTH)
-    } else {
-      raw_tokenized = _.dropRight(raw_tokenized, userText_tokenzied.length).concat(userText_tokenzied)
-    }
-
-    this.setState({ raw_tokenized })
-
-    // turn raw_tokenized into input_tensor
-    const X_vecs = this.one_hot_encode_sequences([raw_tokenized])
-    
-    
-    // Now predict
-    const pred = this.model.predict(X_vecs);
-    pred.print();
-    
-    //  download value from tensor
-    const ans = pred.data()    
-    ans.then(value => {
-      if (this.state.body_text === '') {
-        return this.setState({ score: 0.5000000000000000 })
-      }
-      this.setState({ score: value[0] })
-    })
-
-    // Dispose tensor from memory
-    pred.dispose();    
-  }
-
-  bootUp() {
-    const p1 = tf.loadModel('model/model.json').then(json => {
+    tf.loadModel(XCEPTION_MODEL_PATH).then(json => {
+      console.log('ok!')
+      this.setState({ status_text: 'ok!' })
       console.log(json)
-      return { model: json }
+
+      this.xception = json
+
+      const tmp = this.xception.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3]))
+      tmp.print()
+      tmp.dispose()
     })
-
-    const p2 = fetch( 'word_index.json' ).then(response => response.json()).then(data => {
-      return { word2idx: data }
-    })    
-
-    return Promise.all([p1, p2])
   }
 
-  customLogger() {
-    var logger = document.getElementById('log');
-    console.log = function (message) {
-      if (typeof msg == 'object') {
-        logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
-      } else {
-        logger.innerHTML += message + '<br />';
+  predict(img) {
+
+    const logreg = tf.tidy(() => {
+      // shape [299, 299, 3]
+      let tensor = tf.fromPixels(img).toFloat();
+
+      // Rescale the pixel values (between 0 and 255) to the [0, 1] interval
+      const offset = tf.scalar(255/2)
+      tensor = tensor.sub(offset).div(offset)
+
+      // Reshape to [1, 299, 299, 3]
+      tensor = tensor.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3])
+
+      // now predict
+      return this.xception.predict(tensor);
+    })
+    
+    logreg.data().then(values => {
+      // values is Float32Array(1000)
+      // index = class name, value = prob
+      let classProb = []
+      for (let i = 0; i < values.length; i++) {
+        const prob = values[i];
+        const classIdx = i
+        classProb.push({ classIdx, prob })
       }
-      
-      logger.scrollTop = logger.scrollHeight;
+
+      // sort by prob, get top 3
+      classProb = _.sortBy(classProb, ['prob']).reverse()
+      let topThree = classProb.slice(0, 3)
+
+      // get class name by classIdx
+      topThree = topThree.map(obj => {
+        obj.name = IMAGENET_CLASSES[obj.classIdx]
+        return obj
+      })
+
+      this.setState({ predictions: topThree })
+    })
+  }
+
+  handleFileInput(e) {
+    let files = e.target.files;
+
+    for (let i = 0; i < files.length; i++) {
+      const f = files[i];
+      if (!f.type.match('image.*')) {
+        continue
+      }
+      let reader = new FileReader();
+      reader.onload = e => {
+        this.setState({ image_src: e.target.result })
+
+        // create img element
+        let img = document.createElement('img');
+        img.src = e.target.result;
+        img.width = IMAGE_SIZE;
+        img.height = IMAGE_SIZE;
+        img.onload = () => {
+          this.predict(img)
+        }
+      };
+      reader.readAsDataURL(f);
     }
   }
 
-  componentDidMount(){    
-    this.bootUp().then(values => {
-      this.model = values[0].model;
-      this.word2idx = values[1].word2idx;
-      this.setState({ ready: true })
-    })
+  renderPredictions() {
+    const {
+      predictions
+    } = this.state;
 
-    this.customLogger()
+    if (!predictions.length > 0) {
+      console.log('No predictions found')
+      return
+    }
 
-    window.tf = tf
+    const rows = predictions.map((pred, i) => 
+      <tr className={i === 0 ? 'table-active' : ''}>
+        <td>{pred.name}</td>
+        <td>{Number(Math.round(pred.prob+'e4')+'e-2')} %</td>
+      </tr>
+    )
+
+    return rows
   }
 
   render() {
     const {
-      body_text,
-      raw_tokenized,
-      score
-    } = this.state;
-
+      image_src,
+      status_text,
+      predictions
+    }  = this.state
     return (
-      <div className="App">
-       <div className="jumbotron" style={{ backgroundColor: twitterBackgroundColor, color: 'white' }}>
-          <h1 class="display-4">Sentiment Analysis on Tweets</h1>
-          <p class="lead">Or any other IM messages...</p>
-       </div>
-        
+      <div>
+        <div className="jumbotron">
+          <h1 className="display-4">Image classification</h1>
+          <p className="lead">Learn to name everyday objects</p>
+          <button onClick={this.handleLoadModelClick} type="button" className="btn btn-primary">Load model</button> 
+          <span className='ml-2'>{status_text}</span>
+        </div>
+
         <div className="container">
           <div className="row">
-            <div className="col-7 p-3">
-              <span className="float-left pl-2">What's on your mind ?</span>
-              <textarea className="form-control" value={body_text} onChange={this.handleBodyText} placeholder="So i'm gonna say ...."></textarea>
-            </div>
-            <div className="col-5 p-3">
-              {raw_tokenized && 
-              <div>
-                <b>Message tokenized:</b>  
-                <p>[{raw_tokenized.join(', ')}]</p>
-              </div>}
-            </div>
-
-            {score && 
-                <div className="col-7 p-3">
-                  <b>Sentiment score:</b>
-                  <div className="progress" style={{height: '50px'}}>
-                    <div className="progress-bar" role="progressbar" style={{ width: `${score * 100}%`, fontSize: '24px', backgroundColor: twitterBackgroundColor }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                      {Math.round(score * 100)} %
-                    </div>
-                  </div>
-                </div>
-              }
-
-              <div className="col-5 p-3" style={score ? { visibility: 'visible' } : { visibility: 'hidden' } }>
-                <b>Model logs</b>
-                <div id="log"></div>
+            <div className="input-group mb-3 col-12">
+              <div className="custom-file">
+                <input onChange={this.handleFileInput} type="file" className="custom-file-input" id="inputGroupFile02" />
+                <label className="custom-file-label" htmlFor="inputGroupFile02">Choose file</label>
+              </div>
+              <div className="input-group-append">
+                <span className="input-group-text" id="">Upload</span>
               </div>
             </div>
+
+            {image_src && <div className="text-center col-6"><img src={image_src} className="img-thumbnail" alt="Responsive image" /></div>}
+            {predictions.length > 0 && 
+              <table className="col-6 table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">This is ...</th>
+                    <th scope="col">Of Chance...</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.renderPredictions()}
+                </tbody>
+              </table>}
           </div>
         </div>
+      </div>
     );
   }
 }
