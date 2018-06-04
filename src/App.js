cbb393da533cb6dfb41155f444da0126b9c35573
Richard-NG <richard.chihong.ng@gmail.com>
2018-06-04 18:56:43 +0800

    capture on mouse down

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import PredictionTable from './PredictionTable';
+import './App.css'
 
 const MOBILENET_NOTOP_PATH = 'mobilenet_noTop/model.json'
 const IMAGE_SIZE = 224
@@ -14,21 +15,33 @@ class App extends Component {
       streamTracks: null,
       camReady: false,
 
-      model: null,
       names: ['A', 'B', 'C'],
-      photos: {},
+      photos: {
+        'A': [],
+        'B': [],
+        'C': []
+      },
       probs: {
         'A': 0.33,
         'B': 0.33,
         'C': 0.33
-      }
+      },
     }
 
     this.handleVideo = this.handleVideo.bind(this);
+    this.handleMouseEnd = this.handleMouseEnd.bind(this);
   }
 
   _video = (video) => {
     this.video = video
+  }
+
+  _canvas = (canvas) => {
+    this.canvas = canvas
+  }
+
+  _img = (img) => {
+    this.img = img
   }
 
   handleVideo(stream) {
@@ -77,10 +90,36 @@ class App extends Component {
     this.setUpWebCam()
   }
 
+  handleMouseStart(name) {
+    console.log(name)
+    this.capturing = setInterval(() => {
+      this.canvas.width = IMAGE_SIZE
+      this.canvas.height = IMAGE_SIZE
+
+      const context = this.canvas.getContext('2d')
+      context.drawImage(this.video, 0, 0, IMAGE_SIZE, IMAGE_SIZE)
+      const url = this.canvas.toDataURL('image/jpeg')
+
+      // save the urls, later use them to create img and predict
+      const photos = this.state.photos
+      photos[name].push(url)
+      this.setState({ photos })
+    }, 150)
+  }
+
+  handleMouseEnd() {
+    console.log('mouse up')
+    clearInterval(this.capturing)
+    // this.train()
+  }
+
+
+
   render() {
     const {
       probs,
-      names
+      names,
+      photos
     } = this.state;
 
     return (
@@ -95,31 +134,32 @@ class App extends Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-4">
-              <video autoPlay="true" ref={this._video} width={IMAGE_SIZE} height={IMAGE_SIZE}></video>              
+              <video autoPlay="true" ref={this._video} width={IMAGE_SIZE} height={IMAGE_SIZE}></video>
+              <canvas style={{ display: 'none' }} ref={this._canvas} width={IMAGE_SIZE} height={IMAGE_SIZE}></canvas>
+              <img style={{ display: 'none' }} ref={this._img} alt="dummy"/>
             </div>
-            <div className="col-8">
-              <table className="table table-borderless">
-                <tbody>
-                  <tr style={{ visibility: 'hidden' }}>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                  </tr>
 
+            <div className="col-8">
+                
                   {names.map(name => {
-                    return <tr key={name}>
-                      <td style={{ border: 'none' }}>{name}</td>
-                      <td colSpan="4" style={{ border: 'none' }}>
-                        <div className="progress">
+                    return <div key={name} className="row mb-3">
+                        <div className="col-4">
+                          <button 
+                            onMouseDown={() => this.handleMouseStart(name)}
+                            onMouseUp={this.handleMouseEnd}
+                            className="btn btn-block btn-secondary" 
+                            style={{ height: '90%' }}
+                          >
+                            Class {name} <span className="badge badge-light ml-1">{photos[name].length}</span>
+                          </button>
+                        </div>
+                        <div className="progress col-7 pl-0 mt-2">
                           <div className="progress-bar" role="progressbar" style={{ width: `${probs[name] * 100}%` }} aria-valuenow={`${probs[name]*100}`} aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
-                      </td>
-                    </tr>
+                    </div>
                   })}
-                </tbody>
-              </table>
+                
+                
             </div>
           </div>
         </div>
