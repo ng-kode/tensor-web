bd38d1d4a33aab03b4c563a4c094412bd059c9b5
Richard Ng <richard.ng@corp.agscreative.com>
2018-07-19 15:13:17 +0800

    deployment settings

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker, { unregister } from './registerServiceWorker';
 
 ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+unregister();
